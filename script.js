ca74ea48403551cf24dea7ff65bee14e95d30682
lol-chang <doclecsj@naver.com>
2023-08-25 16:40:11 +0900

    sub page 50%

--- script.js
+++ script.js
@@ -1,22 +1,36 @@
 $(document).ready(function () {
+
   $("#nav-bar1").click(function () {
     $('.features').show();
+    $('.msg').show();
     $('.headline').show();
+    $(".mentor").css("display", "none");
+    $('.mentee').css("display", "none");
   });
 
   $("#nav-bar2").click(function () {
     $('.features').hide();
-    $('.headline').hide();
+    $('.msg').hide();
+    $('.headline').show();
+    $(".mentor").css("display", "none");
+    $('.mentee').css("display", "none");
+    
   });
 
   $("#nav-bar3").click(function () {
-    $('.features').show();
+    $('.features').hide();
+    $('.msg').hide();
     $('.headline').hide();
+    $(".mentor").css("display", "flex");
+    $('.mentee').css("display", "none");
   });
 
   $("#nav-bar4").click(function () {
     $('.features').hide();
-    $('.headline').show();
+    $('.msg').hide();
+    $('.headline').hide();
+    $(".mentor").css("display", "none");
+    $('.mentee').css("display", "flex");
   });
   
   $("#search-icon").click(function() {
